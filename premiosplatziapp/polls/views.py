898897ea449478e3829a1a2ec39a25d3af5be3c8
Wilman2612 <wilman2612@gmail.com>
2022-07-03 18:35:20 -0500

    listado de preguntas

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+   list = Question.objects.all()
+   return render(request, 'polls/index.html', {
+      'latest_question_list' : list
+      })
+
+def detail(request,question_id):
+   obj= get_object_or_404(Question, pk=question_id)
+   return render(request, 'polls/detail.html', {
+      'question' : obj
+      })
+
+def results(request,question_id):
+   return HttpResponse(f"Estás viendo los resultados de la pregunta número{question_id}")
+
+def vote(request,question_id):
+   return HttpResponse(f"Estás votandoala pregunta número{question_id}")
