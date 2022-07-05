a0c496401764af32d65bc754db4c6747b5290dee
Wilman2612 <wilman2612@gmail.com>
2022-07-04 20:57:35 -0500

    habilitada la votacion de preguntas

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,22 +1,35 @@
+import imp
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
+from django.views import generic
 # Create your views here.
 
-def index(request):
-   list = Question.objects.all()
-   return render(request, 'polls/index.html', {
-      'latest_question_list' : list
-      })
+class IndexView(generic.ListView):
+   template_name="polls/index.html"
+   context_object_name="latest_question_list"
+   def get_queryset(self):
+        "Return the last five published questions"
+        return Question.objects.order_by("-pub_date")[:5]
 
-def detail(request,question_id):
-   obj= get_object_or_404(Question, pk=question_id)
-   return render(request, 'polls/detail.html', {
-      'question' : obj
-      })
-
-def results(request,question_id):
-   return HttpResponse(f"Estás viendo los resultados de la pregunta número{question_id}")
+class DetailView(generic.DetailView):
+    model= Question
+    template_name="polls/detail.html"
+class ResultView(generic.DetailView):
+    model= Question
+    template_name="polls/results.html"
 
 def vote(request,question_id):
-   return HttpResponse(f"Estás votandoala pregunta número{question_id}")
+   question=get_object_or_404(Question,pk=question_id)
+   try:
+       selected_choice=question.choice_set.get(pk=request.POST["choice"])
+   except(KeyError,Choice.DoesNotExist):
+       return render(request,"polls/detail.html",{
+           "question":question,
+           "error_message":"No elegiste una respuesta"
+       })             
+   else:
+       selected_choice.votes+=1
+       selected_choice.save()
+       return HttpResponseRedirect(reverse("polls:results",args=(question.id,)))
