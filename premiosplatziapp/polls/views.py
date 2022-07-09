5c76b8332d16083f05524034f14478a991a844fb
Wilman2612 <wilman2612@gmail.com>
2022-07-09 17:14:45 -0500

    Cambios en UI

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -4,18 +4,27 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import Question, Choice
 from django.views import generic
+from django.utils import timezone
 # Create your views here.
 
 class IndexView(generic.ListView):
    template_name="polls/index.html"
    context_object_name="latest_question_list"
+   
    def get_queryset(self):
         "Return the last five published questions"
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model= Question
     template_name="polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes not published questions
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 class ResultView(generic.DetailView):
     model= Question
     template_name="polls/results.html"
